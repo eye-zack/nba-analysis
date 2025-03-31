32f680e492c3c1a7f93abb2870be39c0ece97630
eye-zack <isaacray649@gmail.com>
2025-03-31 10:01:32 -0500

    Added (rough) frontend to repository directory

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <iframe title="NBA 3-PT Analysis" width="1280" height="720" src="https://app.powerbi.com/reportEmbed?reportId=71c2ed4c-b32e-4ad4-8151-fec20492b6e2&autoAuth=true&ctid=19d57598-97b0-442c-b74e-c855b0d87caf" frameborder="0" allowFullScreen="true"></iframe>
+      </header>
+    </div>
+  );
+}
+
+export default App;
